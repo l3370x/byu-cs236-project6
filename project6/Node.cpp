ced934032cf904ef9e8a52734c791ef30c8ae998
l3370x@gmail.com <l3370x@gmail.com@2cafd477-47b3-4fc3-3dd1-bd20daab1095>
2012-12-05 03:22:26 +0000

    submit and passed for real this time

--- project6/Node.cpp
+++ project6/Node.cpp
@@ -8,35 +8,35 @@
 #include "Node.h"
 
 Node::Node() {
-	isQuery = false;
-	number = 1337;
-	visited = false;
-	postorder = 1337;
+    isQuery = false;
+    number = 1337;
+    visited = false;
+    postorder = 1337;
 }
 
 Node::Node(int i , bool query) {
-	isQuery = query;
-	number = i;
-	postorder = 1337;
-	visited = false;
+    isQuery = query;
+    number = i;
+    postorder = 1337;
+    visited = false;
 }
 
 Node::~Node() {
 }
 
 int Node::recurseDG(map<string, Node> & DG, int & i) {
-	if (visited) {
-		return postorder;
-	}
-	visited = true;
-	if (!myChildren.empty()) {
-		set<string>::iterator it;
-		for (it = myChildren.begin() ; it != myChildren.end() ; ++it) {
-			DG[(*it)].postorder = DG[(*it)].recurseDG(DG, i);
-		}
-	}
-	i = i+1;
-	return i;
+    if (visited) {
+        return postorder;
+    }
+    visited = true;
+    if (!myChildren.empty()) {
+        set<string>::iterator it;
+        for (it = myChildren.begin() ; it != myChildren.end() ; ++it) {
+            DG[(*it)].postorder = DG[(*it)].recurseDG(DG, i);
+        }
+    }
+    i = i+1;
+    return i;
 }
 
 vector<int> Node::hasBackwardEdge(map<string, Node> & DG) {
